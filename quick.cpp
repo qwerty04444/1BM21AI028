058a8d6f4b137327efd21c69dcc8836c62d931c2
Ayush <108297189+qwerty04444@users.noreply.github.com>
2023-07-11 09:45:22 +0530

    Update quick.cpp

--- quick.cpp
+++ quick.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
 #include <vector>
 
-// Partition the array and return the index of the pivot element
+
 int partition(std::vector<int>& arr, int low, int high) {
-    int pivot = arr[high];  // Choose the last element as the pivot
-    int i = low - 1;        // Index of the smaller element
+    int pivot = arr[high];  
+    int i = low - 1;       
 
     for (int j = low; j < high; j++) {
         if (arr[j] <= pivot) {
             i++;
-            std::swap(arr[i], arr[j]);  // Swap arr[i] and arr[j]
+            std::swap(arr[i], arr[j]);  
         }
     }
 
-    std::swap(arr[i + 1], arr[high]);  // Swap arr[i+1] and pivot
+    std::swap(arr[i + 1], arr[high]); 
     return i + 1;
 }
 
 // Quick Sort function
 void quickSort(std::vector<int>& arr, int low, int high) {
     if (low < high) {
-        int pivotIndex = partition(arr, low, high);  // Partition the array
+        int pivotIndex = partition(arr, low, high);  
 
-        // Recursively sort the subarrays before and after the pivot
         quickSort(arr, low, pivotIndex - 1);
         quickSort(arr, pivotIndex + 1, high);
     }
@@ -36,7 +35,6 @@ void printArray(const std::vector<int>& arr) {
     std::cout << std::endl;
 }
 
-// Test the quickSort function
 int main() {
     std::vector<int> arr = {9, 3, 17, 1, 25, 6, 11};
     int size = arr.size();
