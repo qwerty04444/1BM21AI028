5338664da4ab512d31f591d8ebc75ad5b5d4b919
Ayush <108297189+qwerty04444@users.noreply.github.com>
2023-07-26 00:51:03 +0530

    Update topological_sort(DFS).cpp

--- topological_sort(DFS).cpp
+++ topological_sort(DFS).cpp
@@ -1,89 +1,115 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-class Graph {
-	int V;
-
-	list<int>* adj;
-
-	void topologicalSortUtil(int v, bool visited[],
-							stack<int>& Stack);
-
-public:
-	Graph(int V);
-	void addEdge(int v, int w);
-
-	void topologicalSort();
+struct edge
+{
+    int deg;
+    int ver[10];
 };
 
-Graph::Graph(int V)
+int node_available(int nodes[],int n)
 {
-	this->V = V;
-	adj = new list<int>[V];
+    for (int i = 0; i < n; i++)
+    {
+        if (nodes[i]!= -1)
+            return i;
+    }
+    return 0;
 }
 
-void Graph::addEdge(int v, int w)
-{
-	adj[v].push_back(w);
+
+bool node_not_in_vn(int vn[],int cn,int n)
+{ 
+    for(int i=0;i<n;i++)
+    {
+        if (cn==vn[i])
+            return 0;
+    }
+    return 1;
 }
-
-void Graph::topologicalSortUtil(int v, bool visited[], stack<int>& Stack)
-{
-	visited[v] = true;
-
-	list<int>::iterator i;
-	for (i = adj[v].begin(); i != adj[v].end(); ++i)
-		if (!visited[*i])
-			topologicalSortUtil(*i, visited, Stack);
-
-	Stack.push(v);
-}
-
-void Graph::topologicalSort()
-{
-	stack<int> Stack;
-
-	bool* visited = new bool[V];
-	for (int i = 0; i < V; i++)
-		visited[i] = false;
-
-	for (int i = 0; i < V; i++)
-		if (visited[i] == false)
-			topologicalSortUtil(i, visited, Stack);
-
-
-	while (Stack.empty() == false) {
-		cout << Stack.top() << " ";
-		Stack.pop();
-	}
-
-	delete [] visited;
-}
-
 
 int main()
 {
-    int n,m,a,b;
+    int n,m,n1,n2,i,j,k,l;
     cout<<"enter number of vertices : ";
     cin>>n;
-    Graph g(n);
+
+    struct edge e[n];
 
     cout<<"enter number of edges : ";
     cin>>m;
 
-    for(int i=0;i<m;i++)
+    int nodes[n];
+
+    for(int i=0;i<n;i++)
     {
-        cout<<"enter 1st vertice of edge "<<i<<" : ";
-        cin>>a;
-        cout<<"enter 2nd vertice of edge "<<i<<" : ";
-        cin>>b;
-        g.addEdge(a,b);
+        cout<<"enter the number of indegree vertices for vertex "<<i<<" : ";
+        cin>>n1;
+        e[i].deg=n1;
+        nodes[i]=i;
+
+        for(int j=0;j<n1;j++)
+        {
+            cout<<"enter the vertex : ";
+            cin>>e[i].ver[j];
+        }
+    }
+
+    int s[n];
+    int top=-1;
+    int vn[n];
+    int vnlen=0;
+    int vnc=0;
+
+    
+
+
+    while (vnlen<n)
+    {
+        if (top==-1)
+        {
+            
+            k=node_available(nodes,n);
+            s[++top]=k;
+            nodes[k]=-1;
+
+        }
+        int cn=s[top];
+
+        if (e[cn].deg==0 && vnlen<n)
+        {
+            vn[vnc++]=cn;
+            top--;
+            cout<<cn;
+        }
+        
+        else
+        {
+            int n1=sizeof(e[cn].ver)/sizeof(e[cn].ver[0]);
+            int flag=0;
+            for (i = 0; i < n1; i++)
+            {
+                l = e[cn].ver[i];
+                if (l >= 0 && l < n && node_not_in_vn(vn, l, n))
+                {
+                    s[++top] = l;
+                    nodes[l] = -1;
+                    flag = 1;
+                    break;
+                }
+            }
+
+
+            if(flag==0 && vnlen<n)
+            {
+                vn[vnc++]=cn;
+                top--;
+                cout<<cn;
+            }
+        }
+        vnlen=vnc;
+        
     }
 
 
-	cout << "Following is a Topological Sort of the given graph \n";
-
-	g.topologicalSort();
-
-	return 0;
 }
