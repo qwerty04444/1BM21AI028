a82f728f076fd6622ec655d7300b5dea4dfc9f9f
Ayush <108297189+qwerty04444@users.noreply.github.com>
2023-08-25 23:06:28 +0530

    Update heapsort.cpp

--- heapsort.cpp
+++ heapsort.cpp
@@ -1,71 +1,61 @@
-
 #include <iostream>
 using namespace std;
 
-
-void heapify(int arr[], int n, int i)
+class heap
 {
-	int largest = i; 
-	int l = 2 * i + 1; 
-	int r = 2 * i + 2; 
+public:
+    void heapify(int arr[], int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
 
-	if (l < n && arr[l] > arr[largest])
-		largest = l;
+        if (left < n && arr[left] > arr[largest])
+          largest = left;
 
-	
-	if (r < n && arr[r] > arr[largest])
-		largest = r;
+        if (right < n && arr[right] > arr[largest])
+          largest = right;
 
+        if (largest != i) {
+          swap(arr[i], arr[largest]);
+          heapify(arr, n, largest);
+        }
+    }
 
-	if (largest != i) {
-		swap(arr[i], arr[largest]);
+    void heapSort(int arr[], int n)
+    {
+        for (int i = n / 2 - 1; i >= 0; i--)
+          heapify(arr, n, i);
+        for (int i = n - 1; i >= 0; i--) {
+          swap(arr[0], arr[i]);
+          heapify(arr, i, 0);
+        }
+    }
 
-	
-		heapify(arr, n, largest);
-	}
-}
+    void printArray(int arr[], int n)
+    {
+        for (int i = 0; i < n; ++i)
+          cout << arr[i] << " ";
+        cout << "\n";
+    }
 
-void heapSort(int arr[], int n)
-{
-	for (int i = n / 2 - 1; i >= 0; i--)
-		heapify(arr, n, i);
-
-	
-	for (int i = n - 1; i >= 0; i--) {
-		
-		swap(arr[0], arr[i]);
-
-		
-		heapify(arr, i, 0);
-	}
-}
-
-void printArray(int arr[], int n)
-{
-	for (int i = 0; i < n; ++i)
-		cout << arr[i] << " ";
-	cout << "\n";
-}
-
+};
 
 int main()
 {
-	int arr[] = { 60 ,20 ,40 ,70, 30, 10};
-	int n = sizeof(arr) / sizeof(arr[0]);
+    heap h;
 
-	for(int i=n/2 -1;i>=0;i--){
-	heapify(arr,n,i);
+    int n;
+    cout<<"enter n : ";
+    cin>>n;
+    int arr[n];
+    cout<<"enter all elements ";
+
+    for (int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    h.heapSort(arr, n);
+
+    cout << "Sorted array is \n";
+    h.printArray(arr, n);
 }
-
-cout << "After heapifying array is \n";
-	printArray(arr, n);
-
-
-	heapSort(arr, n);
-
-	cout << "Sorted array is \n";
-	printArray(arr, n);
-	
-return 0;
-}
-
